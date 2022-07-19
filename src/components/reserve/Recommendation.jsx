e5a854abb281a21c3a852e1d92a0b3a7933c0eb8
Boaz <boaserem022@gmail.com>
2022-07-19 14:01:12 +0300

    quote addition

--- src/components/reserve/Recommendation.jsx
+++ src/components/reserve/Recommendation.jsx
@@ -5,11 +5,11 @@ function Recommendation(){
     return(
         <div>
         <Fade delay={1000} top big cascade>
-          <h1><span>"</span>This is a nice plact to stay. Make reservations now! We are the best in the business.<span>"</span></h1>
+          <h1><span>"</span>We make a living by what we earn but we make life by what we give<span>"</span></h1>
         </Fade> 
         <Fade delay={1500} left>
-            <h3><span>&#x301C;</span>JACK POWER</h3>
-            <p>CEO</p>
+            <h3><span>&#x301C;</span>   WINSTON CHURCHILL</h3>
+            <p>inspirational statesman</p>
         </Fade>
        
       </div>
